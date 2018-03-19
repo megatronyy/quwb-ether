9eca050ba07bf13ccc4cb17cda796d18fcc11e9e
屈文斌 <twfx7758@hotmail.com>
2018-03-19 17:19:41 +0800

    Functions

--- base/pamga.go
+++ base/pamga.go
@@ -6,6 +6,8 @@ import (
 
 func Run(){
 	pointer()
+	any(333)
+	any("666")
 }
 
 func pointer()  {
@@ -18,5 +20,14 @@ func pointer()  {
 	*p = 2
 
 	fmt.Printf("x变量的地址是：%s\n", *p)
+}
+
+func any(v interface{}){
+	fmt.Println("interface{}参数验证")
+	if v1,ok := v.(string); ok{
+		fmt.Println(v1)
+	}else if v1, ok := v.(int); ok{
+		fmt.Println(v1)
+	}
 }
 
--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	//"quwb-ether/functions"
 	//"quwb-ether/typedefs"
-	//"quwb-ether/base"
+	"quwb-ether/base"
 	//"quwb-ether/threads"
-	"quwb-ether/web"
+	//"quwb-ether/web"
 )
 
 func main() {
 	//functions.Run()
 	//typedefs.Run()
-	//base.Run()
+	base.Run()
 	//threads.Run()
-
-	web.Run()
+	//web.Run()
 }
 
