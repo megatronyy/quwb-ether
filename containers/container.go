fda9150c9b278b281b63c3999304f411ffae5254
屈文斌 <twfx7758@hotmail.com>
2018-03-19 17:27:14 +0800

    Functions

--- containers/container.go
+++ containers/container.go
@@ -1,1 +1,20 @@
 package containers
+
+import (
+	"fmt"
+	/*"go/types"
+	"container/list"
+	"container/ring"
+	"container/heap"*/
+)
+
+func Run()  {
+	/*types.Array{}
+	types.Slice{}
+	types.Map{}
+	list.List{}
+	ring.Ring{}
+	heap.Init()*/
+
+	fmt.Println("容器类型Demo")
+}
