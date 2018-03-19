7dcf4e5c9282b091107b0fe9369a7ce239447626
屈文斌 <twfx7758@hotmail.com>
2018-03-19 17:40:22 +0800

    Functions

--- containers/container.go
+++ containers/container.go
@@ -9,12 +9,14 @@ import (
 )
 
 func Run()  {
-	/*types.Array{}
-	types.Slice{}
-	types.Map{}
-	list.List{}
-	ring.Ring{}
-	heap.Init()*/
+	/*
+		types.Array{} //数组
+		types.Slice{} //切片
+		types.Map{} //map
+		list.List{} //双向链表
+		ring.Ring{} //循环双向链表
+		heap.Init()
+	*/
 
 	fmt.Println("容器类型Demo")
 }
