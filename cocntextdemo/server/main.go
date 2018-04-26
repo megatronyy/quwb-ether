95eee1285c161072e8f009f06debe7f1275ace69
屈文斌 <twfx7758@hotmail.com>
2018-04-26 16:31:23 +0800

    Context

--- cocntextdemo/server/main.go
+++ cocntextdemo/server/main.go
@@ -5,7 +5,10 @@ import (
 	"math/rand"
 	"time"
 	"fmt"
+	"context"
 )
+
+const requestIDKey = "rid"
 
 func lazyHandler(w http.ResponseWriter, r *http.Request) {
 	ranNum := rand.Intn(2)
@@ -20,7 +23,33 @@ func lazyHandler(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+func newContextWithRequestID(ctx context.Context, req *http.Request) context.Context {
+	reqID := req.Header.Get("X-Request-ID")
+	if reqID == "" {
+		reqID = "0"
+	}
+	return context.WithValue(ctx, requestIDKey, reqID)
+}
+
+func requestIDFromContext(ctx context.Context) string {
+	return ctx.Value(requestIDKey).(string)
+}
+
+func middleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		ctx := newContextWithRequestID(req.Context(), req)
+		next.ServeHTTP(w, req.WithContext(ctx))
+	})
+}
+
+func h(w http.ResponseWriter, req *http.Request)  {
+	reqID := requestIDFromContext(req.Context())
+	fmt.Fprintln(w, "Request ID: ", reqID)
+	return
+}
+
 func main() {
-	http.HandleFunc("/", lazyHandler)
+	http.HandleFunc("/lazy", lazyHandler)
+	http.Handle("/middle", middleWare(http.HandlerFunc(h)))
 	http.ListenAndServe(":9201", nil)
 }
