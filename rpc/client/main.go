b6c384cb5de14319ad971a7bcf439a478a4c5a2e
屈文斌 <twfx7758@hotmail.com>
2018-05-04 17:03:23 +0800

    Context

---
+++ rpc/client/main.go
@@ -0,0 +1,34 @@
+package client
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	pb "quwb-ether/rpc"
+	"os"
+	"context"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+	name := defaultName
+	if len(os.Args) >1 {
+		name = os.Args[1]
+	}
+
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatal("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
